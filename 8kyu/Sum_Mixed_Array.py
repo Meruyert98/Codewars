d8d25dcb6c57cab4f060f0fadc11b54ff33d1050
Slamova Meruyert <40336212+Meruyert98@users.noreply.github.com>
2023-11-13 23:23:29 +0600

    Create Sum_Mixed_Array.py

---
+++ 8kyu/Sum_Mixed_Array.py
@@ -0,0 +1,15 @@
+#Sum Mixed Array
+
+"""
+DESCRIPTION:
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+"""
+
+def sum_mix(arr):
+    #your code here
+    sum = 0
+    for i in arr:
+        sum +=int(i)
+    return sum
